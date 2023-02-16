340def03f7acf39380ded928f947125d29d02c44
Daniel Ansari <daniel.ansari.2023@gmail.com>
2023-02-16 15:31:41 +0700

    fix: add validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,6 @@
 class Product < ApplicationRecord
-  monetize :price_cents
+  monetize :price_cents, numericality: { greater_than: 0 }
+
+  validates :title, presence: true
+  validates :image_url, format: URI::DEFAULT_PARSER.make_regexp(%w[http https])
 end
